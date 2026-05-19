f424a3c2f43ba4b9d7a85f4c3a416423977212dd
sofya0206 <faraforar@gmail.com>
2026-05-19 19:40:47 +0700

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,17 +1,25 @@
-import { createClient } from "@supabase/supabase-js"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get("code")
+  const next = requestUrl.searchParams.get("next") ?? "/"
 
   if (code) {
+    const { createClient } = await import("@supabase/supabase-js")
     const supabase = createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        auth: {
+          persistSession: true,
+          autoRefreshToken: true,
+          detectSessionInUrl: true,
+        }
+      }
     )
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect("https://chess-c-rho.vercel.app")
+  return NextResponse.redirect(new URL("/", requestUrl.origin))
 }
